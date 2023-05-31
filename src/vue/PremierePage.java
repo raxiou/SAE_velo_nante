8826574fba21a5e41508d77e2f84ae843c8b4654
raxiou <Raxiou80@mail.com>
2023-05-31 17:10:10 +0200

    passage en gridBagLayout

--- src/vue/PremierePage.java
+++ src/vue/PremierePage.java
@@ -4,7 +4,7 @@ import javax.swing.border.Border;
 
 import java.awt.*;
 
-public class PremierePage extends JFrame{
+public class PremierePage extends JFrame {
     private JLabel cPresentation;
     private JLabel lPresentation;
     private JLabel petitLabel;
@@ -22,7 +22,6 @@ public class PremierePage extends JFrame{
     //private PremierePageListener listener;
 
     public PremierePage() {
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.initComponents();
     }
@@ -32,8 +31,8 @@ public class PremierePage extends JFrame{
         this.lPresentation = new JLabel("<html>BIKE vous permet de consulter de nombreuses données relatives au passage de vélos dans la ville de Nantes, et de visualiser ces données par zone définies.</html>");
         this.petitLabel = new JLabel("Se connecter :");
 
-        Font titre = new Font("Arial",Font.BOLD,32);
-        Font texte = new Font("Arial",Font.BOLD,24);
+        Font titre = new Font("Arial", Font.BOLD, 32);
+        Font texte = new Font("Arial", Font.BOLD, 24);
 
         this.cPresentation.setFont(titre);
         this.cPresentation.setHorizontalAlignment(JLabel.CENTER);
@@ -54,7 +53,6 @@ public class PremierePage extends JFrame{
         this.connexion = new JButton("Connexion");
         this.connexion.setSize(new Dimension(200, 50));
 
-
         ImageIcon logoE = new ImageIcon("../data/image/logoEntreprise.png");
         this.logoEntreprise = new JLabel(logoE);
 
@@ -63,77 +61,78 @@ public class PremierePage extends JFrame{
         this.logoVille = new JLabel(new ImageIcon(imageResize));
         this.logoVille.setHorizontalAlignment(JLabel.LEFT);
 
-
-
-        // Mise en place d'un layout de type GridLayout
-        GridLayout gridLayout = new GridLayout(7, 3);
-        gridLayout.setHgap(10); // Espacement horizontal entre les composants
-        gridLayout.setVgap(10); // Espacement vertical entre les composants
-        this.setLayout(gridLayout);
+        // Mise en place d'un layout de type GridBagLayout
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        this.setLayout(gridBagLayout);
 
         // Création d'une bordure vide avec une couleur de votre choix
         Color gridColor = Color.BLACK;
         int borderWidth = 2;
         Border gridBorder = BorderFactory.createLineBorder(gridColor, borderWidth);
 
-        // Ajout des composants avec des bordures pour les grilles
-        this.logoVille.setBorder(gridBorder);
-        this.logoEntreprise.setBorder(gridBorder);
-        this.cPresentation.setBorder(gridBorder);
-        this.lPresentation.setBorder(gridBorder);
-        this.petitLabel.setBorder(gridBorder);
-        this.champNom.setBorder(gridBorder);
-        this.champMDP.setBorder(gridBorder);
-        this.creatCompte.setBorder(gridBorder);
-        this.coAsInv.setBorder(gridBorder);
-        this.connexion.setBorder(gridBorder);
-
-
-        // Créez un GridBagConstraints pour le premier bouton et spécifiez la largeur et la hauteur à 2
+        // Création d'un GridBagConstraints pour les composants
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.insets = new Insets(10, 10, 10, 10); // Espacement entre les composants
 
-        this.add(this.logoVille);
-        this.add(this.logoEntreprise);
-        this.add(new JLabel());
-        this.add(new JLabel());
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        this.logoVille.setBorder(gridBorder);
+        this.add(this.logoVille, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        this.logoEntreprise.setBorder(gridBorder);
+        this.add(this.logoEntreprise, gridBagConstraints);
+
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        this.cPresentation.setBorder(gridBorder);
         this.add(this.cPresentation, gridBagConstraints);
-        this.add(new JLabel());
 
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 3;
+        this.lPresentation.setBorder(gridBorder);
+        this.add(this.lPresentation, gridBagConstraints);
 
-        this.add(new JLabel());
-        this.add(this.lPresentation);
-        this.add(new JLabel());
-        this.add(new JLabel());
-        this.add(this.petitLabel);
-        this.add(new JLabel());
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        this.petitLabel.setBorder(gridBorder);
+        this.add(this.petitLabel, gridBagConstraints);
 
+        gridBagConstraints.gridy = 3;
+        this.champNom.setBorder(gridBorder);
+        this.add(this.champNom, gridBagConstraints);
 
-        this.add(new JLabel());
-        this.add(this.champNom);
-        this.add(this.creatCompte);
-        this.add(new JLabel());
-        this.add(this.champMDP);
-        this.add(this.coAsInv);
+        gridBagConstraints.gridy = 4;
+        this.champMDP.setBorder(gridBorder);
+        this.add(this.champMDP, gridBagConstraints);
 
-        this.add(new JLabel());
-        this.add(this.connexion);
-        this.add(new JLabel());
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.gridwidth = 1;
+        this.creatCompte.setBorder(gridBorder);
+        this.add(this.creatCompte, gridBagConstraints);
+
+        gridBagConstraints.gridx = 2;
+        this.coAsInv.setBorder(gridBorder);
+        this.add(this.coAsInv, gridBagConstraints);
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        this.connexion.setBorder(gridBorder);
+        this.add(this.connexion, gridBagConstraints);
 
         this.pack();
-
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                PremierePage laFrame  = new PremierePage();
-                laFrame.pack();
+                PremierePage laFrame = new PremierePage();
                 laFrame.setVisible(true);
             }
         });
-
     }
-
-
 }
