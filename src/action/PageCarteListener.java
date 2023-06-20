f02402976aaf8b3a36e20c652969a5dfbbbab53d
Raxiou <raxiou80@gmail.com>
2023-06-20 10:07:06 +0200

    premier page listeneur

---
+++ src/action/PageCarteListener.java
@@ -0,0 +1,37 @@
+package action;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import vue.PageAccueil;
+import vue.PremierePage;
+
+public class PageCarteListener implements ActionListener {
+    private JFrame frame;
+
+    public PageCarteListener(JFrame frame) {
+        this.frame = frame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent paramActionEvent) {
+        // recupere le nom du bouton
+        String nomBouton = paramActionEvent.getActionCommand();
+
+        // si le bouton est "Confirmer"
+        if (nomBouton.equals("Se deconnecter")) {
+            // on affiche la page d'Acueil
+            
+            PremierePage laFrame  = new PremierePage();
+            laFrame.pack();
+            laFrame.setSize(new Dimension(1920, 1080));
+            laFrame.setVisible(true);
+            this.frame.setVisible(false);
+                        
+        }
+    }
+    
+}
